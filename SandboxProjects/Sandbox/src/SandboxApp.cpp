afd5beafed61ceab38c1b9f4b272974b159a04ba
JuanJose Gomez <gjuanjo1999@gmail.com>
2024-06-16 13:43:50 +0200

    Camera controll added via imgui widgets and fix the glitches due the alpha of the texture (fixed with the alpha testing)

--- SandboxProjects/Sandbox/src/SandboxApp.cpp
+++ SandboxProjects/Sandbox/src/SandboxApp.cpp
@@ -6,7 +6,6 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-//TODO Arreglar el depth buffer no trabaja con las texturas transparentes y hace cosas raras con el fondo
 //TODO A veces crashea el buffer al cargar el modelado
 //TODO Valorar si compensa hacer una plantilla AssetLibrary<T>
 //TODO Revisar sistema de coordenadas (si son cm o m)
@@ -62,11 +61,12 @@ class ExampleLayer : public Taller::Layer {
 public:
 	ExampleLayer() : Layer("Example") {
 
-		/* CREATES A TRIANGLE ENTITY */
+		/* CREATES A AXOLOTL ENTITY */
 		Taller::Entity triangle = coord.CreateEntity();
 		coord.AddComponent<Taller::TransformComponent>(triangle, glm::vec3(0.0f), glm::vec3(-45.0f), glm::vec3(0.25f));
 		coord.AddComponent<Taller::StaticMeshComponent>(triangle, "TriangleShader", "axolotl", "cyan_texture");
 		Taller::StaticMeshComponent& axolotl_SM = coord.GetComponent<Taller::StaticMeshComponent>(triangle);
+		coord.GroupEntity(triangle,"Movable");
 
 		m_Texture2DLibrary.Load(axolotl_SM.textureName, "assets/textures/axolotl_cyan.png");
 		m_MeshLibrary.Load(axolotl_SM.meshName, "assets/3dmodels/axolotl.tmesh");
@@ -74,6 +74,16 @@ public:
 		//----------------------------------//
 
 
+		/* CREATES A GROUND PLANE ENTITY */
+		Taller::Entity plane = coord.CreateEntity();
+		coord.AddComponent<Taller::TransformComponent>(plane, glm::vec3(0.0f, -0.5f, 0.0f), glm::vec3(90.0f, 0.0f, 0.0f), glm::vec3(15.0f));
+		coord.AddComponent<Taller::StaticMeshComponent>(plane, "SquareShader", "", "square");
+		Taller::StaticMeshComponent& plane_SM = coord.GetComponent<Taller::StaticMeshComponent>(plane);
+		coord.GroupEntity(plane, "Static");
+
+		m_MeshLibrary.Load(plane_SM.meshName, "assets/3dmodels/Square.tmesh");
+		m_ShaderLibrary.Load(plane_SM.shaderName, "assets/shaders/squareColor.glsl");
+		//----------------------------------//
 
 
 		/* CREATES A SQUARE ENTITY */
@@ -81,12 +91,12 @@ public:
 		coord.AddComponent<Taller::TransformComponent>(square, glm::vec3(-2.0f, 0.0f, 0.0f), glm::vec3(-55.0f, 10.0f, 45.0f), glm::vec3(0.25f));
 		coord.AddComponent<Taller::StaticMeshComponent>(square);
 		Taller::StaticMeshComponent& cube_SM = coord.GetComponent<Taller::StaticMeshComponent>(square);
+		coord.GroupEntity(square, "Movable");
 
 		cube_SM.meshName = "Cube";
 		m_MeshLibrary.Load(cube_SM.meshName, "assets/3dmodels/Cube.tmesh");
 
 		cube_SM.shaderName = "SquareShader";
-		m_ShaderLibrary.Load(cube_SM.shaderName, "assets/shaders/squareColor.glsl");
 		//----------------------------------//
 
 
@@ -124,20 +134,34 @@ public:
 			Taller::RenderCommand::Clear();
 		}
 		
+		for (auto entities : coord.GetEntitiesByGroup("cam")) {
+				transformCam.location.x = camx;
+				transformCam.location.y = camy;
+				transformCam.location.z = camz;
+
+				transformCam.rotation.x = glm::degrees(camRotx);
+				transformCam.rotation.y = glm::degrees(camRoty);
+				transformCam.rotation.z = glm::degrees(camRotz);
+
+
+				//TODO	This mantains an scale and proportion relative to aspect ration, implement this inside the renderer
+				float aspectRation = ((float)(Taller::Application::Get().GetWindow().GetWidth()) / (float)(Taller::Application::Get().GetWindow().GetHeight()));
+				cam.projection = glm::perspective(glm::radians(45.0f), aspectRation, 0.1f, 100.0f);
+				//--------------------------------------
+
+				//TODO Implement this in the renderer in each frame because the camera will be always transforming
+				cam.viewProjection = Taller::ComponentOperations::CalculateCameraViewProjection(cam, transformCam);
+		}
+
 		for (auto entities : coord.QueryEntitiesBySignature(renderSignature)) {
 			Taller::TransformComponent& transform = coord.GetComponent<Taller::TransformComponent>(entities.GetId());
 			Taller::StaticMeshComponent& staticMesh = coord.GetComponent<Taller::StaticMeshComponent>(entities.GetId());
 
-			transform.rotation.x = glm::degrees(x);
-			transform.rotation.y = glm::degrees(y);
-			transform.rotation.z = glm::degrees(z);
-
-			//TODO	This mantains an scale and proportion relative to aspect ration, implement this inside the renderer
-			float aspectRation = ((float)(Taller::Application::Get().GetWindow().GetWidth()) / (float)(Taller::Application::Get().GetWindow().GetHeight()));
-			cam.projection = glm::perspective(glm::radians(45.0f), aspectRation, 0.1f, 100.0f);
-			//--------------------------------------
-
-			cam.viewProjection = Taller::ComponentOperations::CalculateCameraViewProjection(cam, transformCam);
+			if (coord.EntityBelongsToGroup(entities, "Movable")) {
+				transform.rotation.x = glm::degrees(x);
+				transform.rotation.y = glm::degrees(y);
+				transform.rotation.z = glm::degrees(z);
+			}
 
 			{
 				TL_PROFILE_SCOPE("Render");
@@ -174,8 +198,22 @@ public:
 		ImGui::SliderAngle("X", &x);
 		ImGui::SliderAngle("Y", &y);
 		ImGui::SliderAngle("Z", &z);
+		ImGui::Separator();
 
 		ImGui::End();
+
+		ImGui::Begin("Camera controls!");
+
+		ImGui::SliderFloat("X", &camx, -100.0f, 100.0f, "X = %f");
+		ImGui::SliderFloat("Y", &camy, -100.0f, 100.0f, "Y = %f");
+		ImGui::SliderFloat("Z", &camz, -100.0f, 100.0f, "Z = %f");
+
+		ImGui::SliderAngle("ROT X", &camRotx);
+		ImGui::SliderAngle("ROT Y", &camRoty);
+		ImGui::SliderAngle("ROT Z", &camRotz);
+
+		ImGui::End();
+		
 		
 	}
 
@@ -195,6 +233,13 @@ private:
 	float x;
 	float y;
 	float z;
+
+	float camx = 0;
+	float camy = 1.0;
+	float camz = 5.0;
+	float camRotx = 0;
+	float camRoty = 0;
+	float camRotz = 0;
 };
 
 class Sandbox : public Taller::Application {
